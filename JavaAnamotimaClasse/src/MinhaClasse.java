ed3a79cd8008e3b9e395991a0632f26a7b95fc54
VitorMariano-dev <vitor.vitormariano@outlook.com>
2024-04-30 14:59:41 -0300

    Entendendo sobre declaração de variáveis

--- JavaAnamotimaClasse/src/MinhaClasse.java
+++ JavaAnamotimaClasse/src/MinhaClasse.java
@@ -1,7 +1,26 @@
 public class MinhaClasse {
 
 	public static void main (String [] args) {
-		System.out.print("Hello word");
+		// System.out.print("Hello word"); // First Class
+		// final String BR = "Brasil"; // Váriavel Maiuscula é considerada como final e não sofre mais nenhuma alteração
+		
+		/* DECLARAÇÃO INVÁLIDA DE VARIÁVEIS
+		 * 
+		 * int numero&um = 1; // Os únicos símbolos permitidos são _ e $
+		 * int 1numero = 1; // Uma variável não pode comecar com númerico
+		 * int numero um = 1; // Não pode ter espaço no nome da variável
+		 * int long = 1; //long faz parte das palavras reservadas da linguagem
+		 */
+		
+		/* DECLARAÇÃO VÁLIDA DE VARIÁVEIS
+		 * 
+		 * int numero$um = 1;
+		 * int numero1 = 1;
+		 * int numeroum = 1;
+		 * int longo = 1;
+		 */
+		
+		
 	}
 	
 }
--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 Repositório criado para armazenar e documentar todo o conteudo do Modulo de Java básico e treinar versionamento
 
 Toda alteração será realizado via pull request e subirei via Git
+
+## Primeira Aula
+
+Nesta primeira aula preparamos o ambiente de desenvolvimento e entendemos como que funciona o public class e imprimimos no console o primeiro "Hello word"
+
+## Segunda aula
+
+Nesta segunda aula passamos a entender como declarar variáveis de forma válida e também entendi que quando declaramos uma variável em maiusculo ela passa a ser considerada uma variável final e não sofre nenhum tipo de alteração.
+
+
+### OBS: Tudo estudado durante as aulas estou deixando comentado dentro do código com exemplos.
